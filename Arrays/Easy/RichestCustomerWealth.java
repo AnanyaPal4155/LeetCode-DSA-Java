1b0bf674a7eab7afa67e67cbac9bc52d63cd2524
Ananya <ananyapal8227@gmail.com>
2026-09-23 12:43:57 +0530

    Solve Richest Customer Wealth

--- Arrays/Easy/RichestCustomerWealth.java
+++ Arrays/Easy/RichestCustomerWealth.java
@@ -1,3 +1,34 @@
 public class RichestCustomerWealth {
 
+    public static void main(String[] args) {
+
+        int[][] accounts = {
+                {2, 8, 1},
+                {5, 3, 4},
+                {7, 1, 6}
+        };
+
+        System.out.println(maximumWealth(accounts));
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+
+        int max = Integer.MIN_VALUE;
+
+        for (int person = 0; person < accounts.length; person++) {
+
+            int sum = 0;
+
+            for (int account = 0; account < accounts[person].length; account++) {
+
+                sum += accounts[person][account];
+            }
+
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        return max;
+    }
 }
