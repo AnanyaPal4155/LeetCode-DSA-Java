292e57d80c470c0f658d03264b73f682a99515e7
Ananya <ananyapal8227@gmail.com>
2026-09-24 10:44:50 +0530

    BinarySearch

---
+++ Binary-Search/Easy/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr =  {1,4, 5,6,7,8,9,10};
+        int target = 7;
+        System.out.println(Search(arr, target));
+    }
+
+    public static int  Search(int[] arr, int target){
+          int start = 0;
+          int end = arr.length -1;
+
+        while (start <= end){
+               int mid = (start + end)/2;
+               if( target < arr[mid]){
+                   end = mid - 1;
+               }else if(target > arr[mid]){
+                    start = mid + 1;
+               }
+               else {
+                   return mid;
+               }
+           }
+
+           return -1;
+    }
+}
